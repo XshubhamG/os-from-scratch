d3bcd15828df358d7272175b54131cc503f44f66
Shubham Giri <shubhammgiri3@gmail.com>
2025-10-07 00:11:03 +0530

    implemented user mode application testing and run our first application as user

--- kernel.c
+++ kernel.c
@@ -15,6 +15,9 @@ extern char __free_ram[], __free_ram_end[];
 extern char __kernel_base[];
 extern char __stack_top[];
 
+// symbols for embedded shell binary
+extern char _binary_shell_bin_start[], _binary_shell_bin_size[];
+
 // --- Forward Declarations ---
 void kernel_entry(void);
 void proc_a_entry(void);
@@ -164,8 +167,17 @@ void map_page(uint32_t *table1, uint32_t vaddr, paddr_t paddr, uint32_t flags) {
 
 // --- Process Management ---
 
+// ↓ __attribute__((naked)) is very important!
+__attribute__((naked)) void user_entry(void) {
+  __asm__ __volatile__("csrw sepc, %[sepc]        \n"
+                       "csrw sstatus, %[sstatus]  \n"
+                       "sret                      \n"
+                       :
+                       : [sepc] "r"(USER_BASE), [sstatus] "r"(SSTATUS_SPIE));
+}
+
 // Creates a new process with its own stack and page table.
-struct process *create_process(uint32_t pc) {
+struct process *create_process(const void *image, size_t image_size) {
   struct process *proc = NULL;
   int i;
   for (i = 0; i < PROCS_MAX; i++) {
@@ -180,25 +192,42 @@ struct process *create_process(uint32_t pc) {
 
   // Set up the initial stack with callee-saved registers.
   uint32_t *sp = (uint32_t *)&proc->stack[sizeof(proc->stack)];
-  *--sp = 0;            // s11
-  *--sp = 0;            // s10
-  *--sp = 0;            // s9
-  *--sp = 0;            // s8
-  *--sp = 0;            // s7
-  *--sp = 0;            // s6
-  *--sp = 0;            // s5
-  *--sp = 0;            // s4
-  *--sp = 0;            // s3
-  *--sp = 0;            // s2
-  *--sp = 0;            // s1
-  *--sp = 0;            // s0
-  *--sp = (uint32_t)pc; // ra (return address)
+  *--sp = 0;                    // s11
+  *--sp = 0;                    // s10
+  *--sp = 0;                    // s9
+  *--sp = 0;                    // s8
+  *--sp = 0;                    // s7
+  *--sp = 0;                    // s6
+  *--sp = 0;                    // s5
+  *--sp = 0;                    // s4
+  *--sp = 0;                    // s3
+  *--sp = 0;                    // s2
+  *--sp = 0;                    // s1
+  *--sp = 0;                    // s0
+  *--sp = (uint32_t)user_entry; // ra (return address)
 
   // Allocate and map the page table.
   uint32_t *page_table = (uint32_t *)alloc_pages(1);
+
+  // map the kernel pages (identity mapping)
   for (paddr_t paddr = (paddr_t)__kernel_base; paddr < (paddr_t)__free_ram_end;
        paddr += PAGE_SIZE)
     map_page(page_table, paddr, paddr, PAGE_R | PAGE_W | PAGE_X);
+
+  // Map user pages.
+  for (uint32_t off = 0; off < image_size; off += PAGE_SIZE) {
+    paddr_t page = alloc_pages(1);
+
+    // Handle the case where the data to be copied is smaller than the
+    // page size.
+    size_t remaining = image_size - off;
+    size_t copy_size = PAGE_SIZE <= remaining ? PAGE_SIZE : remaining;
+
+    // Fill and map the page.
+    memcpy((void *)page, image + off, copy_size);
+    map_page(page_table, USER_BASE + off, page,
+             PAGE_U | PAGE_R | PAGE_W | PAGE_X);
+  }
 
   // Initialize process fields.
   proc->pid = i + 1;
@@ -336,19 +365,18 @@ void kernel_main(void) {
   // Clear the BSS section.
   memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
 
-  printf("");
+  printf("\n\n");
 
   // Set the trap vector.
   WRITE_CSR(stvec, (uint32_t)kernel_entry);
 
   // Create the idle process.
-  idle_proc = create_process((uint32_t)NULL);
+  idle_proc = create_process(NULL, 0);
   idle_proc->pid = 0; // pid=0 means idle
   current_proc = idle_proc;
 
-  // Create example processes.
-  proc_a = create_process((uint32_t)proc_a_entry);
-  proc_b = create_process((uint32_t)proc_b_entry);
+  // new!
+  create_process(_binary_shell_bin_start, (size_t)_binary_shell_bin_size);
 
   // Start the scheduler.
   yield();
